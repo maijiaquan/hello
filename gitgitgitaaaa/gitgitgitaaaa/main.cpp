9830f9da851d77fe7ce1f1b0f9aa500765a98e61
Xiaoming Mai <Michael@maixiaomingdeMacBook-Air.local>
2016-03-23 16:55:10 +0800

    laizhende!!!

---
+++ gitgitgitaaaa/gitgitgitaaaa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitgitgitaaaa
+//
+//  Created by 麦小明 on 16/3/23.
+//  Copyright (c) 2016年 麦小明. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;   //hahahhahahahahloloooojasdlfjajskfd;jal;ksfjdkljasklfjl;a
+}
